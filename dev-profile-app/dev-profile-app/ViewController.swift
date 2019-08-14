d842e696de5a19e34eab42ba924a39490390c153
zeraMe1ster <sauhard.eg@gmail.com>
2019-08-14 14:32:19 +0545

    initialized application

---
+++ dev-profile-app/dev-profile-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile-app
+//
+//  Created by Sau on 8/14/19.
+//  Copyright © 2019 Sau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
